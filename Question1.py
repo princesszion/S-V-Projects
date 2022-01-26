2d166f1a81ed63c5ebe7c86ddd6944334fd26da6
Ghea Sandrine <s.ghea1@alustudent.com>
2022-01-26 11:32:31 +0200

    task1

---
+++ Question1.py
@@ -0,0 +1,10 @@
+import time
+Start_time = time.time()
+num1 = 12
+num2 = 10
+AddNumbers = num1 + num2
+print(AddNumbers)
+End_time = time.time()
+Total_time = End_time - Start_time
+print("Time: ", Total_time)
+
