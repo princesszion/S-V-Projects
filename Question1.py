e41f40b5130e93b90d6684e3a5c08c48cda77863
Ghea Sandrine <s.ghea1@alustudent.com>
2022-01-27 23:22:51 +0200

    question2

--- Question1.py
+++ Question1.py
@@ -2,7 +2,10 @@ import time
 Start_time = time.time()
 num1 = 12
 num2 = 10
-AddNumbers = num1 + num2
+num3 = 20
+num4 = 50
+num5 = 1000000
+AddNumbers = num1 + num2 + num3 + num4 + num5
 print(AddNumbers)
 End_time = time.time()
 Total_time = End_time - Start_time
