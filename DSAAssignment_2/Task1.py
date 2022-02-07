f694531c936317409e005a85d48fae270bb007bf
Ghea Sandrine <s.ghea1@alustudent.com>
2022-02-07 12:41:28 +0200

    task1

--- DSAAssignment_2/Task1.py
+++ DSAAssignment_2/Task1.py
@@ -2,17 +2,22 @@
 # It returns true if all elements of the list occur an odd number of times.
 # Otherwise it returns false
 def checkduplicates():
+    #Getting yer input
     user_input = input("Please enter items in the list ")
+    #Divide a string(user_input) into a list
     list1 = user_input.split(" ")
-    even = []
-    odd = []
-    for element in list1:
-        if int(list1.count(element)) % 2 == 0:
-            even.append(element)
-            if len(even) > 0:
-               return False
-        else:
-            return True
+    even_number = []
+    for element in (list1):
+        if int(list1.count(element)) % 2 == 0 and element not in even_number:
+            even_number.append(element)
+    if len(even_number) > 0:
+        # print(even_number)
+        return  False
+    else:
+        return True
+        # print("Odd numbers")
+
+
 
 
 print(checkduplicates())
